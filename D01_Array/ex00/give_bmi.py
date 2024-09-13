d3b9ae2403bdcc13156a18550963b14827363180
catal mathis <catalmathis@gmail.com>
2024-09-13 16:35:22 +0200

    debutPremier

---
+++ D01_Array/ex00/give_bmi.py
@@ -0,0 +1,9 @@
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    lenght = len(height)
+    if len(height) > len(weight):
+        lenght = len(weight)
+    return [weight[i] / height[i] ** 2 for i in range(lenght)]
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    return [True if i > limit else False for i in bmi]
