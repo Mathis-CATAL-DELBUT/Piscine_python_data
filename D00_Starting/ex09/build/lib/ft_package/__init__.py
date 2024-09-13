f3439e083387e4c39543d3fdc0485da5ba38aa32
catal mathis <catalmathis@gmail.com>
2024-09-13 16:09:17 +0200

    9 okk

--- D00_Starting/ex09/build/lib/ft_package/__init__.py
+++
@@ -1,1 +0,0 @@
-from ft_package.count_in_list import count_in_list
--- D00_Starting/ex09/build/lib/ft_package/count_in_list.py
+++
@@ -1,5 +0,0 @@
-def count_in_list(lst, value):
-    """
-    Count the number of occurrences of a value in a list.
-    """
-    return lst.count(value)
