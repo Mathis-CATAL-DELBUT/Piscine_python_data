52d91172bf1f3e21a6a79eda5c0bd58165f64aca
catal mathis <catalmathis@gmail.com>
2024-09-13 15:15:40 +0200

    fini

---
+++ D00_Starting/ex09/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ftpackage",
+    version="0.0.1",
+    author="mcatal-d",
+    author_email="mcatal-d@student.42.fr",
+    description="A sample test package",
+    packages=find_packages(),
+    install_requires=[],
+    license="MIT",
+    url="https://github.com/eagle/ft_package",
+)
